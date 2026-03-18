98f443c96d018bf6a0b4a21c52769ceee7bf401b
Sukumar dey <dey.sukumar1999@gmail.com>
2026-03-18 19:41:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let btn = document.getElementById("btn")
+let output = document.getElementById("output")
+
+async function print(){
+let text = document.getElementById("text").value
+let delay = document.getElementById("delay").value
+
+	await  new Promise((res,rej) => setTimeout(res, Number(delay)))
+	console.clear()
+	console.log(text,delay)
+	output.textContent = text
+}
+
+btn.addEventListener("click",print)
